a34752b065e18d2d5dc4bd6b5c2ab55f09ddc22e
victor_marquez <victorsheepman@gmail.com>
2024-05-09 15:43:27 -0400

    Initial Commit

---
+++ PokemonApp/PokemonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonAppApp.swift
+//  PokemonApp
+//
+//  Created by Victor Marquez on 9/5/24.
+//
+
+import SwiftUI
+
+@main
+struct PokemonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
