ac92db2afc2520636bf1d0c32efa74b3f081e619
victor_marquez <victorsheepman@gmail.com>
2024-05-13 19:22:50 -0400

    added header home title

--- PokemonApp/ContentView.swift
+++ PokemonApp/ContentView.swift
@@ -15,8 +15,24 @@ struct ContentView: View {
         NavigationView{
             ZStack{
                 Color("Color/Primary").edgesIgnoringSafeArea(.all)
-             
+                VStack{
+                    HStack(spacing:12){
+                        Image("Pokeball")
+                            .renderingMode(.template)
+                               .resizable()
+                               .frame(width: 24, height: 24)
+                               .foregroundColor(.white)
+                        Text("Pokédex")
+                            .fontWeight(.bold)
+                            .font(.system(size: 32))
+                            .foregroundStyle(.white)
+                        Spacer()
+                    }
+                    Spacer()
+                }.padding(.horizontal, 18)
+                
             }
+           
         }
             
     }
