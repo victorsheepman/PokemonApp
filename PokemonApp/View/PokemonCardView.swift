3935adfca95c2af145b91f2b7f756fb19a23d674
victor_marquez <victorsheepman@gmail.com>
2024-05-15 15:25:23 -0400

    view

--- PokemonApp/View/PokemonCardView.swift
+++ PokemonApp/View/PokemonCardView.swift
@@ -10,16 +10,46 @@ import SwiftUI
 struct PokemonCardView: View {
     var body: some View {
         ZStack{
+            Color(.red).edgesIgnoringSafeArea(.all)
             Rectangle()
-                .frame(width: 104, height: 108)
+                .frame(width: 110, height: 108)
                 .cornerRadius(8)
-                .foregroundColor(.blue)
+                .foregroundColor(.white)
                 .overlay(
-                    Rectangle()
-                        .frame(width: 104, height: 44) // Mitad del tamaño
-                        .foregroundColor(.white)
-                        .padding(.top, 64)
+                    ZStack {
+                        Rectangle()
+                            .frame(width: 110, height: 44)
+                            .clipShape(
+                                .rect(
+                                topLeadingRadius: 7,
+                                bottomLeadingRadius: 8,
+                                bottomTrailingRadius: 8,
+                                topTrailingRadius: 7
+                                )
+                            )
+                            .padding(.top, 64)
+                        .foregroundColor(Color("Color/BackgroundGray"))
+                        
+                        VStack(spacing:0){
+                            Text("#9999")
+                                .font(.system(size: 10))
+                                .foregroundStyle(Color("Color/MediumGray"))
+                                .fontWeight(.light)
+                                .padding(.leading,55)
+                            Image("Silhouette")
+                                .resizable()
+                                .renderingMode(.template)
+                                .frame(width: 72, height: 72)
+                                .foregroundStyle(Color("Color/MediumGray"))
+                            Text("Pokemon Name")
+                                .font(.system(size: 12))
+                                .fontWeight(.medium)
+                                .foregroundStyle(Color("Color/DarkGray"))
+                        }
+                    }
                 )
+           
+                
         }
     }
 }
