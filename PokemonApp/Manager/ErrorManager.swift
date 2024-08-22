9709a7c9494f934909c445eb35e16c27648886b2
Victor Marquez <72099625+victorsheepman@users.noreply.github.com>
2024-08-22 11:18:56 -0400

    progress

--- PokemonApp/Manager/ErrorManager.swift
+++ PokemonApp/Manager/ErrorManager.swift
@@ -12,14 +12,14 @@ class ErrorManager {
 
     func handler(_ error: NetworkError) {
         switch error {
-        case .invalidURL:
-            print("Error: La URL es inválida.")
-        case .requestFailed(let underlyingError):
-            print("Error: La solicitud falló con error: \(underlyingError.localizedDescription)")
-        case .invalidResponse:
-            print("Error: La respuesta del servidor no es válida.")
-        case .decodingError(let decodingError):
-            print("Error: Falló la decodificación de los datos con error: \(decodingError.localizedDescription)")
+            case .invalidURL:
+                print("Error: La URL es inválida.")
+            case .requestFailed(let underlyingError):
+                print("Error: La solicitud falló con error: \(underlyingError.localizedDescription)")
+            case .invalidResponse:
+                print("Error: La respuesta del servidor no es válida.")
+            case .decodingError(let decodingError):
+                print("Error: Falló la decodificación de los datos con error: \(decodingError.localizedDescription)")
         }
     }
 }
