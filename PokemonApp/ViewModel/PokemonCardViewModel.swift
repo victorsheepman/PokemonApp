05a15cbcf35be4918f3b5e153b6632b39b550c6c
Victor Marquez <72099625+victorsheepman@users.noreply.github.com>
2024-08-22 10:59:23 -0400

    refacto ring

--- PokemonApp/ViewModel/PokemonCardViewModel.swift
+++ PokemonApp/ViewModel/PokemonCardViewModel.swift
@@ -10,19 +10,44 @@ import Combine
 
 class PokemonCardViewModel: ObservableObject {
     private var cancellables = Set<AnyCancellable>()
-    private let pokemonSeletectedApi = PokemonSelectedApi()
+
     @Published var pokemonSprite:String = ""
     @Published var pokemonTag: Int  = 0
     
-    func fetchPokemonDetail(pokemonUrl:String){
-        pokemonSeletectedApi.fetchData(url: pokemonUrl)
+    func fetchPokemonDetail(pokemonUrl: String) {
+        guard let url = URL(string: pokemonUrl) else {
+            self.handleError(.invalidURL)
+            return
+        }
+        
+        NetworkManager.shared.fetchData(from: url, responseType: PokemomSelectedModel.self)
             .receive(on: DispatchQueue.main)
-            .sink(receiveCompletion: {_ in}){ data in
-                self.pokemonSprite = data.sprites.front_default ?? ""
-                print(data.order)
-                self.pokemonTag = data.order
+            .sink { completion in
+                switch completion {
+                case .finished:
+                    break
+                case .failure(let error):
+                    self.handleError(error)
+                }
+            } receiveValue: { [weak self] dataModel in
+                self?.pokemonSprite = dataModel.sprites.front_default ?? ""
+                self?.pokemonTag = dataModel.order
             }
             .store(in: &cancellables)
+    }
+    
+    
+    private func handleError(_ error: NetworkError) {
+        switch error {
+        case .invalidURL:
+            print("Error: La URL es inválida.")
+        case .requestFailed(let underlyingError):
+            print("Error: La solicitud falló con error: \(underlyingError.localizedDescription)")
+        case .invalidResponse:
+            print("Error: La respuesta del servidor no es válida.")
+        case .decodingError(let decodingError):
+            print("Error: Falló la decodificación de los datos con error: \(decodingError.localizedDescription)")
+        }
     }
     
     
